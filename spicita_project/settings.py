3fac287fc726bbff36da38c28d627d82736b2dcd
CrusaderGoT <enememeka44@gmail.com>
2026-03-23 22:55:55 +0100

    add storages for django files

--- spicita_project/settings.py
+++ spicita_project/settings.py
@@ -166,7 +166,9 @@ STATIC_URL = "/static/"
 # Static file serving.
 # https://whitenoise.readthedocs.io/en/stable/django.html#add-compression-and-caching-support
 STORAGES = {
-    # ...
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
     "staticfiles": {
         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
     },
