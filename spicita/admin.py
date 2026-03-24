639ac11fb07d8886f9e7a18a0a67beb7bb36dab2
Emeka <133652764+CrusaderGoT@users.noreply.github.com>
2026-03-24 12:14:54 +0100

    Update admin.py
    added extra item and order item to admin.py

--- spicita/admin.py
+++ spicita/admin.py
@@ -44,12 +44,46 @@ class AdminExtra(admin.ModelAdmin):
             dish.extras.add(obj)
 
 
+class OrderItemExtraInline(admin.TabularInline):
+    model = OrderItemExtra
+    extra = 0
+    fields = ["extra", "quantity"]
+
+
+class AdminOrderItem(admin.ModelAdmin):
+    model = OrderItem
+    list_display = ["dish", "order", "quantity"]
+    search_fields = ["dish__name", "order__ticket"]
+    list_filter = ["dish"]
+    inlines = [OrderItemExtraInline]
+
+    def delete_queryset(self, request: HttpRequest, queryset: QuerySet[OrderItem]) -> None:
+        order_item_ids = list(queryset.values_list("id", flat=True))
+        OrderItemExtra.objects.filter(order_item_id__in=order_item_ids).delete()
+        super().delete_queryset(request, queryset)
+
+
+class AdminOrderItemExtra(admin.ModelAdmin):
+    model = OrderItemExtra
+    list_display = ["extra", "order_item", "quantity"]
+    search_fields = ["extra__name", "order_item__dish__name"]
+    list_filter = ["extra"]
+
+
+class OrderItemInline(admin.TabularInline):
+    model = OrderItem
+    extra = 0
+    fields = ["dish", "quantity"]
+    show_change_link = True
+
+
 class AdminOrder(admin.ModelAdmin):
     model = Order
     list_display = ["ticket", "customer", "total_price", "ordered", "delivered", "date"]
     list_filter = ["ordered", "delivered", "date"]
     search_fields = ["ticket", "customer__user__username"]
     readonly_fields = ["total_price", "ticket", "date"]
+    inlines = [OrderItemInline]
 
     def save_model(
         self, request: HttpRequest, obj: Order, form: Any, change: bool
@@ -68,4 +102,6 @@ class AdminOrder(admin.ModelAdmin):
 
 admin.site.register(Dish, AdminDish)
 admin.site.register(Extra, AdminExtra)
+admin.site.register(OrderItem, AdminOrderItem)
+admin.site.register(OrderItemExtra, AdminOrderItemExtra)
 admin.site.register(Order, AdminOrder)
